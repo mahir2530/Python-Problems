25eb436dbaa14d2de371bb0cde0418020252935e
Mahir Patel <patelmahir820@gmail.com>
2024-08-04 12:49:29 +0530

    Create Problem 23 Break, Continue and Pass Statement.py

---
+++ Problem 23 Break, Continue and Pass Statement.py
@@ -0,0 +1,18 @@
+
+print('====BREAK====')
+for letter in 'Python':
+    if letter == 'h':
+        break  
+    print("Letter:", letter)
+
+print('====CONTINUE====')
+for letter in 'Python':
+    if letter == 'h':
+        continue  
+    print("Letter:", letter)
+
+print('====PASS====')
+for letter in 'Python':
+    if letter == 'h':
+        pass  
+    print("Letter:", letter)
