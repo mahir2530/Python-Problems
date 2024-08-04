bad3817e541fc52d36ef9930f881d3f13c594aa1
Mahir Patel <patelmahir820@gmail.com>
2024-08-04 11:40:35 +0530

    Create Problem 14 Range.py

---
+++ Problem 14 Range.py
@@ -0,0 +1,13 @@
+print(list(range(0)))
+
+print(list(range(1,10)))
+
+start=2
+stop=14
+step=1
+print(list(range(start,stop,step)))
+
+start1=2
+stop1=-14
+step1=-2
+print(list(range(start1,stop1,step1)))
