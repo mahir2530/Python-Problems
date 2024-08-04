6ff13971cd0da362c276ea7488a1a103502f276f
Mahir Patel <patelmahir820@gmail.com>
2024-08-04 11:35:18 +0530

    Create Problem 11 Identity.py

---
+++ Problem 11 Identity.py
@@ -0,0 +1,16 @@
+x1=5
+y1=5
+x2='hello'
+y2='hello'
+x3=[1,2,3]
+y3=[1,2,3]
+x4=(1,2,3)
+y4=(1,2,3)
+print("ID OF X1")
+print(id(x1))
+print("ID OF X2")
+print(id(x2))
+print(x1 is y1)
+print(x2 is y2)
+print(x3 is y3)
+print(x4 is y4)
