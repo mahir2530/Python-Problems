f1e04ceac92f42e7cfe6904bc15095bd36f83e48
Mahir Patel <patelmahir820@gmail.com>
2024-08-04 11:36:30 +0530

    Create Problem 12 List Function.py

---
+++ Problem 12 List Function.py
@@ -0,0 +1,9 @@
+list=['hello',5687,56.879,'rahul',99.45]
+tinylist=[56,'gujrat']
+
+print(list)          #print complate list
+print(list[0])       #print First element of list
+print(list[1:3])     #print elements starting from 2nd till 3rd of list
+print(list[2:])      #print elements starting from 2nd till end of list
+print(tinylist*2)    #print list two time
+print(list+tinylist) #print concatenated list
