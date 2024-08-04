041e49a630ad01873884105ff7c67005b3800d6f
Mahir Patel <patelmahir820@gmail.com>
2024-08-04 11:29:58 +0530

    Create Problem 6 List.py

---
+++ Problem 6 List.py
@@ -0,0 +1,15 @@
+fruits=[]
+
+a=str(input("enter a fruit :- " ))
+b=str(input("enter a fruit :- " ))
+c=str(input("enter a fruit :- " ))
+d=str(input("enter a fruit :- " ))
+e=str(input("enter a fruit :- " ))
+
+fruits.append(a)
+fruits.append(b)
+fruits.append(c)
+fruits.append(d)
+fruits.append(e)
+
+print("the fruits :- " ,fruits)
